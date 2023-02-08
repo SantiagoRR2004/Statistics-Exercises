82f47efe77f4280427cb9e08641da8188ebab79c
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2023-02-08 12:00:00 +0100

    Formatting

--- ClasedeR1.R
+++ ClasedeR1.R
@@ -3,10 +3,10 @@
 x <- 1:10
 mean(x)
 x
-mean <- function(x=1:10) {
-  media2 <- base::mean(c(x,1))
-  media <- (sum(x)+1)/(length(x)+1)
-  return(list(media,media2) )
+mean <- function(x = 1:10) {
+  media2 <- base::mean(c(x, 1))
+  media <- (sum(x) + 1) / (length(x) + 1)
+  return(list(media, media2))
 }
 
 l <- mean()
@@ -20,26 +20,30 @@ an <- function(a1, r, n) {
 }
 
 x <- 5
-ifelse(x%%2==0, paste(x," par"), paste(x,"impar"))
+ifelse(x %% 2 == 0, paste(x, " par"), paste(x, "impar"))
 
 arquivos <- list.files()
-for (arq in arquivos){
+for (arq in arquivos) {
   print(arq)
 }
 
-outer(1:10,1:10,"*")
+outer(1:10, 1:10, "*")
 
 mifuncion <- function(x) {
   return(sqrt(sum(1:x)))
 }
-sapply(1:10, mifuncion )
+sapply(1:10, mifuncion)
 
 
 library(carData)
 data(AMSsurvey)
 
-write.table(AMSsurvey, file="proba.txt", sep="\t",
-            row.names=FALSE, col.names=TRUE)
+write.table(
+  AMSsurvey,
+  file = "proba.txt",
+  sep = "\t",
+  row.names = FALSE,
+  col.names = TRUE
+)
 
-dd <- read.table("proba.txt", header=TRUE, sep="\t", dec=".")
-
+dd <- read.table("proba.txt", header = TRUE, sep = "\t", dec = ".")
